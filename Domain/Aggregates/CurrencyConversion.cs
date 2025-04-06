df206e04e65322511183d8201f3efcb9e8829674
Vinicius Evangelista <vini.evangelista@live.com>
2025-04-06 13:03:51 -0300

    feat: upgrade to .NET 9.0 and add currency conversion domain model

---
+++ Domain/Aggregates/CurrencyConversion.cs
@@ -0,0 +1,29 @@
+using Domain.ValueObjects.cs;
+
+namespace Domain.Aggregates;
+
+public class CurrencyConversion
+{
+    public Guid Id { get; set;  }
+    public Currency SourceCurrency { get; set;  }
+    public Currency TargetCurrency { get; set;  }
+    public decimal Rate { get; set;  }
+    public DateTimeOffset RetrievedAt { get; set; }
+    
+    public  CurrencyConversion() { }
+
+    public CurrencyConversion(Currency sourceCurrency, Currency targetCurrency, decimal rate, DateTime retrievedAt)
+    {
+        Id = Guid.NewGuid();
+        SourceCurrency = sourceCurrency;
+        TargetCurrency = targetCurrency;
+        Rate = rate;
+        RetrievedAt = retrievedAt;
+    }
+
+    public void UpdateRate(decimal newRate, DateTime retrievedAt)
+    {
+        Rate = newRate;
+        RetrievedAt = retrievedAt;
+    }
+}
